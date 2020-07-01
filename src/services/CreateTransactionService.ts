4c66d929bdd79b1daa2e1d1808bd8a610627600f
João Vítor Marquim <jvmsmfn@cin.ufpe.br>
2020-07-01 16:01:24 -0300

    Implementa o serviço para criação de uma nova transação

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,43 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+import FindOrCreateCategoryService from './FindOrCreateCategoryService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const FindOrCreateCategory = new FindOrCreateCategoryService();
+
+    const newCategory = await FindOrCreateCategory.execute({ title: category });
+
+    const category_id = newCategory.id;
+
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
