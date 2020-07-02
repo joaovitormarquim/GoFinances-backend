e4aec170e2797752a23823f52e3d67055da7302d
João Vítor Marquim <jvmsmfn@cin.ufpe.br>
2020-07-02 11:40:19 -0300

    Checa se o saldo é suficiente

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -5,6 +5,7 @@ import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
 import CategoriesRepository from '../repositories/CategoriesRepository';
+import AppError from '../errors/AppError';
 
 interface Request {
   title: string;
@@ -29,6 +30,12 @@ class CreateTransactionService {
     const category_id = newCategory.id;
 
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const balance = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw new AppError('Your balance is isuficient', 400);
+    }
 
     const transaction = await transactionsRepository.create({
       title,
